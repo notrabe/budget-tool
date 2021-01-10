8b532810bfd6c6db5855f7d54989c43ed96510c7
Evan Barton <notrabe1011@gmail.com>
2021-01-10 18:58:57 -0500

    started work on back end, set up server, began routes and controllers

---
+++ server.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const express = require('express')
+const colors = require('colors')
+const morgan = require('morgan')
+
+const app = express()
+
+const transactionsRouter = require('./routes/transactions-router')
+
+app.use('/api/transactions', transactionsRouter)
+
+const PORT = process.env.PORT || 5000
+
+app.get('/', (req, res) => res.send('Hello'))
+
+app.listen(PORT, console.log(`Server is running on port ${PORT}`.blue.bold))
