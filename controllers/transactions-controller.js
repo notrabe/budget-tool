a8bc1b871fba80be86606e4cf0268bce6621252e
Evan Barton <notrabe1011@gmail.com>
2021-01-10 21:27:23 -0500

    made transaction schema, continued work on controller

--- controllers/transactions-controller.js
+++ controllers/transactions-controller.js
@@ -1,11 +1,27 @@
-exports.getTransactions = (req, res, next) => {
-    res.send('GET transactions')
-}
+const Transaction = require('../models/transactions-model')
 
-exports.addTransaction = (req, res, next) => {
+exports.getTransactions = async (req, res, next) => {
+    try {
+      const transactions = await Transaction.find();
+  
+      return res.status(200).json({
+        success: true,
+        count: transactions.length,
+        data: transactions
+      });
+    } catch (err) {
+      return res.status(500).json({
+        success: false,
+        error: 'Something went wrong.'
+      });
+    }
+  }
+  
+
+exports.addTransaction = async (req, res, next) => {
     res.send('POST transaction')
 }
 
-exports.deleteTransaction = (req, res, next) => {
+exports.deleteTransaction = async (req, res, next) => {
     res.send('DELETE transactions')
 }
