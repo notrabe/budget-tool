d97dfdf3b504d9fbd7c54bb214442cf3e50787be
Evan Barton <notrabe1011@gmail.com>
2021-01-11 20:53:04 -0500

    started implementation of back end into front end

--- client/src/context/GlobalState.js
+++ client/src/context/GlobalState.js
@@ -1,16 +1,33 @@
 import React, {createContext, useReducer} from 'react'
 import AppReducer from './AppReducer'
+import axios from 'axios'
 
 const initialState = {
-    transactions: [
-        
-    ]
+    transactions: [],
+    error: null,
+    loading: true
 }
 
 export const GlobalContext = createContext(initialState)
 
 export const GlobalProvider = ({children}) => {
     const [state, dispatch] = useReducer(AppReducer, initialState)
+
+    async function getTransactions() {
+        try {
+            const res = await axios.get('/api/transactions')
+
+            dispatch({
+                type: 'GET_TRANSACTIONS',
+                payload: res.data.data
+            })
+        } catch (err) {
+            dispatch({
+                type: 'TRANSACTION_ERROR',
+                payload: err.response.data.error
+            })
+        }
+    }
 
     function deleteTransaction(id){
         dispatch({
@@ -28,8 +45,11 @@ export const GlobalProvider = ({children}) => {
 
     return (<GlobalContext.Provider value={{
         transactions: state.transactions,
+        error: state.error,
+        loading: state.loading,
         deleteTransaction,
-        addTransaction
+        addTransaction,
+        getTransactions
     }}>
         {children}
             </GlobalContext.Provider>)
