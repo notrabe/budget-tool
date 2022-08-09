ba4689a3977eeeb78f521cc0c574e4b05e9132dd
Evan Barton <notrabe1011@gmail.com>
2022-08-09 06:45:11 -0400

    got app up and running again.

--- config/db-config.js
+++ config/db-config.js
@@ -1,18 +1,23 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const uri =
+  'mongodb+srv://user1:user1password@budget-tool.qk0za.mongodb.net/?retryWrites=true&w=majority';
+require('dotenv').config();
 
 const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useCreateIndex: true,
-            useUnifiedTopology: true
-        })
+  try {
+    const conn = await mongoose.connect(uri, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+    });
 
-        console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline.bold)
-    } catch(err) {
-        console.log(`Error: ${err.message}`.red)
-        process.exit(1)
-    }
-}
+    console.log(
+      `MongoDB connected: ${conn.connection.host}`.cyan.underline.bold
+    );
+  } catch (err) {
+    console.log(`Error: ${err.message}`.red);
+    process.exit(1);
+  }
+};
 
-module.exports = connectDB
+module.exports = connectDB;
